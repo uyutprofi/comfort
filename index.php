413c8e8470b8dec06f9da55542be40f9d0832953
uyutprofi <evgeniyyzhaninov@gmail.com>
2020-01-16 14:40:57 +0200

    added disqus comments

--- index.php
+++ index.php
@@ -472,19 +472,27 @@
         var sn = d.getElementsByTagName(s)[0]; sn.parentNode.insertBefore(gcw, sn);
     })(document, window, 'script');
     </script>
-    <script type="text/javascript">
-        _hcwp = window._hcwp || [];
-        _hcwp.push({widget:"Stream", widget_id: 91643, social: "google, facebook, twitter, tumblr, livejournal"});
-        (function() {
-            if("HC_LOAD_INIT" in window)return;
-            HC_LOAD_INIT = true;
-            var lang = (navigator.language || navigator.systemLanguage || navigator.userLanguage || "en").substr(0, 2).toLowerCase();
-            var hcc = document.createElement("script"); hcc.type = "text/javascript"; hcc.async = true;
-            hcc.src = ("https:" == document.location.protocol ? "https" : "http")+"://w.hypercomments.com/widget/hc/91643/"+lang+"/widget.js";
-            var s = document.getElementsByTagName("script")[0];
-            s.parentNode.insertBefore(hcc, s.nextSibling);
-        })();
-    </script>
-    <script src="assets/main.min.js"></script>
+  <div id="disqus_thread"></div>
+  <script>
+
+      /**
+       *  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
+       *  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables*/
+      /*
+      var disqus_config = function () {
+      this.page.url = PAGE_URL;  // Replace PAGE_URL with your page's canonical URL variable
+      this.page.identifier = PAGE_IDENTIFIER; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
+      };
+      */
+      (function() { // DON'T EDIT BELOW THIS LINE
+          var d = document, s = d.createElement('script');
+          s.src = 'https://oknaremont-kiev-ua.disqus.com/embed.js';
+          s.setAttribute('data-timestamp', +new Date());
+          (d.head || d.body).appendChild(s);
+      })();
+  </script>
+  <noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
+  <script src="assets/main.min.js"></script>
+  <script id="dsq-count-scr" src="//oknaremont-kiev-ua.disqus.com/count.js" async></script>
 </body>
 </html>
